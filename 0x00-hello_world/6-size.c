743041276e790b4aab8bb6a2446bbd60f9b2c5b1
jenntang1 <1039@holbertonschool.com>
2019-09-18 19:24:24 +0000

    Understanding basic C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-int main()
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-
-
-
-	return 0;
-
+	printf("Size of a char: %d\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(char));
+	printf("Size of a long int: %d\n", sizeof(char));
+	printf("Size of a long long int: %d\n", sizeof(char));
+	printf("Size of a float: %d\n", sizeof(char));
+	return (0);
 }
