4ecbee8a713fe64479d1153feee9fed5cd3e552c
jenntang1 <1039@holbertonschool.com>
2019-09-18 18:50:13 +0000

    Understanding basic C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
+/**
+ *This document uses the puts function to write out a character string
+ */
 #include <stdio.h>
-
-int main()
-
+/**
+ *The main function is very necessary in C programming
+ */
+int main(void)
 {
-
-
-
-
-	return 0;
-
+	puts("Programming is like building a multilingual puzzle \n");
+	/**
+	 *This return function will return a value 0
+	 */
+	return (0);
 }
