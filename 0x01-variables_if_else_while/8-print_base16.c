d8f324391dad3fcf25d811531c93c416a30fb2bf
jenntang1 <1039@holbertonschool.com>
2019-09-19 21:59:42 +0000

    Understanding basic C programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char j;
+
+	for(j = '0'; j <= '9'; j++)
+		putchar(j);
+		putchar('x');
+		putchar('\n');
+	return (0);
+}
