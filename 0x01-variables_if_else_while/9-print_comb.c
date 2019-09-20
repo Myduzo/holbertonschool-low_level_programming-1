d8dad412724b74bcca4e3fbe0cc7a327e2a514ce
jenntang1 <1039@holbertonschool.com>
2019-09-20 21:06:13 +0000

    Understanding basic C programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,13 @@ int main(void)
 
 	for (j = '0'; j <= '9'; j++)
 	{
-		if (j < '9')
-			putchar(j);
+		putchar(j);
+
+		if (j <= '8')
+		{
 			putchar(',');
 			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
