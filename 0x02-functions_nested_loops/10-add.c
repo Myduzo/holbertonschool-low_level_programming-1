ad4faef9635b9eb458275adb1dadc7c015d32cc3
jenntang1 <1039@holbertonschool.com>
2019-09-25 01:21:39 +0000

    Understanding functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - add two integers
+ * @n: int
+ * @j: int
+ * Description: Add integers and return with statement
+ * Return: Always 0.
+ */
+int add(int n, int j)
+{
+	return (n + j);
+}
