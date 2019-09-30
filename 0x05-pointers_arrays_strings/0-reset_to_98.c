051941f05bf4381b4a1273de48cf2715601d2dbc
jenntang1 <1039@holbertonschool.com>
2019-09-30 20:21:08 +0000

    Understanding pointers, arrays and strings in C

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,12 @@
 #include "holberton.h"
 
-
+/**
+ * reset_to_98 - function updates pointer value to 98
+ * @n: integers
+ * Description: value from 402 to 98
+ * Return: Always 0 is success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
