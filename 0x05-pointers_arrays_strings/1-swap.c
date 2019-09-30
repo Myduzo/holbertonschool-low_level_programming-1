1948536f9cd66b61f4e57067ae6350cc34d85007
jenntang1 <1039@holbertonschool.com>
2019-09-30 20:37:02 +0000

    Understanding pointers, arrays and strings in C

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - function that swaps values
+ * @a: integers
+ * @b: integers
+ * Description: Swaps two integers
+ * Return: Always 0 is success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = 98;
+}
