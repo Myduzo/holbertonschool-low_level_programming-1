60e5275d4fc64ab48a1d68be59fa850ae9d98fb6
jenntang1 <1039@holbertonschool.com>
2019-10-01 17:53:01 +0000

    Understanding pointers, arrays and strings in C

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,18 +3,17 @@
 /**
  * rev_string - function that reverses a string
  * @s: characters
- * Description: Reverses a string
+ * Description: Prints a string in reverse
  * Return: Always 0 is success
  */
 void rev_string(char *s)
 {
 	char reverse;
 	int start = 0;
-	int end = 10;
+	int end = '\0';
 
-	reverse = s[start];
 	s[start] = s[end];
+	reverse = s[start];
 	s[end] = reverse;
-	start++;
-	end--;
+	start--;
 }
