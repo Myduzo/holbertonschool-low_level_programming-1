876c19efbfa56a76bad2ca797359f64f604d4700
jenntang1 <1039@holbertonschool.com>
2019-10-01 08:41:58 +0000

    Understanding pointers, arrays and strings in C

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: characters
+ * Description: Look up man puts for reference
+ * Return: Always 0 is success
+ */
+void _puts(char *str)
+{
+	_putchar(*str);
+}
