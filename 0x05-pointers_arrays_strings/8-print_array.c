5fa5d193d10f51e4554691c57ee51e84d2310d63
jenntang1 <1039@holbertonschool.com>
2019-10-01 20:36:02 +0000

    Understanding pointers, arrays and strings in C

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements
+ * @a: integers
+ * @n: integers
+ * Description: Prints n elements of an array
+ * Return: Always 0 is success
+ */
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (count < n)
+	{
+		if (count != n)
+		{
+			printf("%d, ", a[count]);
+			count++;
+		}
+	}
+	printf("\n");
+}
