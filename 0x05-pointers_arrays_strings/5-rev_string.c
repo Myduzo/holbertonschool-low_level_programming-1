32b3d09eb2ca2cb91c6883dcdbda287fd2af5cc4
jenntang1 <1039@holbertonschool.com>
2019-10-02 01:09:02 +0000

    Understanding pointers, arrays and strings in C

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,12 +8,46 @@
  */
 void rev_string(char *s)
 {
-	char reverse;
-	int start = 0;
-	int end = '\0';
+	int len;
+	int count;
 
-	s[start] = s[end];
-	reverse = s[start];
-	s[end] = reverse;
-	start--;
+	char *start;
+	char *end;
+	char ph;
+
+	len = _strlen(s);
+	start = s;
+	end = s;
+
+	for (count = 0; count < len - 1; count++)
+	{
+		end++;
+	}
+	for (count = 0; count < len / 2; count++)
+	{
+		ph = *end;
+		*end = *start;
+		*start = ph;
+
+		start++;
+		end--;
+	}
+}
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: characters
+ * Description: Look up man strlen for reference
+ * Return: Always 0 is success
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		len++;
+	}
+	return (len);
 }
