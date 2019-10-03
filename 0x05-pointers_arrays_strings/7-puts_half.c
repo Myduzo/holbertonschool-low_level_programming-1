87541f1a15d139ae84571bb5cd07d15087dc9082
jenntang1 <1039@holbertonschool.com>
2019-10-03 03:15:26 +0000

    Understanding pointers, arrays and strings in C

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,4 +8,33 @@
  */
 void puts_half(char *str)
 {
+	/* initialized the full len, first half & second half */
+	int len = _strlen(str);
+	int first = len / 2;
+	int second;
+
+	/* loops through the second half only & end before null */
+	for (second = first; second != '\0'; second++)
+	{
+		_putchar(str[second]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: characters
+ * Description: Look up man strlen for reference
+ * Return: Always 0 is success
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		len++;
+	}
+	return (len);
 }
