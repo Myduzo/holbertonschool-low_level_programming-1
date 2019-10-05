1656cbcea7bdaac8f7fb4485846b536aceeb8243
jenntang1 <1039@holbertonschool.com>
2019-10-05 13:16:41 +0000

    Understanding basic C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints stderr
  * Description: Printing a standard error message
- * Return: Always 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
-	perror ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+    write (2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+    return (1);
 }
