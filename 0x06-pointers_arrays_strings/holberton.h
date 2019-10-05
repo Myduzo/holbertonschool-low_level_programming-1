961b919ad6da6aeb2d9605c139dc275afd34c96f
jenntang1 <1039@holbertonschool.com>
2019-10-05 03:09:44 +0000

    Understanding pointers, arrays and strings in C

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,7 +2,7 @@
 #define HOLBERTON_HFILE
 
 int _putchar(char c);
-char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -11,5 +11,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
 char *leet(char *);
+void print_number(int n);
 
 #endif
