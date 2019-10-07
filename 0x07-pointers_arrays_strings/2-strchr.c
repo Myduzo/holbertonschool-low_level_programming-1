8f84c96e491d04f1f61164960dbc6f7598b5f9fa
jenntang1 <1039@holbertonschool.com>
2019-10-07 15:18:46 +0000

    Understanding double pointers and multi-dimensional arrays

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - function locates a char in a string
+ * @s: letters
+ * @c: letters
+ * Description: Returns pointers to the first occurrence of chr in a string
+ * Return: Pointer to char
+ */
+char *_strchr(char *s, char c)
+{
+    /* declare iteration variable */
+	int iterate;
+
+	/* declare temporary pointer */
+	char *locate = s;
+
+	/* look through the entire string to the null */
+	for (iterate = 0; s[iterate] != '\0'; iterate++)
+	{
+		/* find char in string */
+		if (s[iterate] == c)
+		{
+			/* take the memory address of the char */
+			locate = &s[iterate];
+			/* returns pointer to char */
+			return (locate);
+		}
+	}
+	/* returns null */
+	return ('\0');
+}
