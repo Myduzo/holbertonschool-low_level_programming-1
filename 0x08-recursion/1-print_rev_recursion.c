d9bf2e79816b2f1f2d634c56ad4f70784c6c14b8
jenntang1 <1039@holbertonschool.com>
2019-10-09 17:02:52 +0000

    Understanding Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: letters
+ * Description: Look up man puts for reference
+ * puts function automatically adds a new line
+ * Return: a char string
+ */
+void _print_rev_recursion(char *s)
+{
+	/* if pointer is not null byte */
+	if (*s != '\0')
+	{
+		/* move the pointer to the next index */
+		_print_rev_recursion(s + 1);
+		/* print the string */
+		_putchar(*s);
+	}
+}
