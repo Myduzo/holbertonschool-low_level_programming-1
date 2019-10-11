1ead17864492d446de594b13f70945a80ab4073d
jenntang1 <1039@holbertonschool.com>
2019-10-11 02:39:38 +0000

    Understanding Recursion

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -10,18 +10,26 @@
  */
 int helper(char *s, int n, int i)
 {
+	/* BASE CASE */
+	/* if there is only one char */
 	if (n == i)
 	{
+		/* then, it's still a palindrome */
 		return (1);
 	}
+	/* if first & last chars don't match */
 	if (s[n] != s[i])
 	{
+		/* then, return 0 because no match found */
 		return (0);
 	}
+	/* if there's more than 2 chars */
 	if (n < i + 1)
 	{
+		/* recurse from start and end of line */
 		return (helper(s, n + 1, i - 1));
 	}
+	/* return 1 if all else fails */
 	return (1);
 }
 
@@ -29,16 +37,13 @@ int helper(char *s, int n, int i)
  * is_palindrome - function that cheks for a palindrome string
  * @s: characters
  * Description: check for a palindrome string
- * Return: 1
+ * Return: 1 for palindrome & 0 is not
  */
 int is_palindrome(char *s)
 {
+	/* initialize n to get the length of the string */
 	int n = _strlen_recursion(s);
-
-	if (n == 0)
-	{
-		return (1);
-	}
+	/* check from 0 */
 	return (helper(s, 0, n - 1));
 }
 
