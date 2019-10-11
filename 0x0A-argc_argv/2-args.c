bd2304304bf026a490fc8d845f1ed09ac2495a8f
jenntang1 <1039@holbertonschool.com>
2019-10-11 17:19:19 +0000

    Understanding arguments to main

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Description: Write a program that prints all arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	/* declare a counter */
+	int iterate;
+
+	/* if there is something in the array of strings */
+	/* other than the program name */
+	if (argc > 1)
+	{
+		/* loop through the array of strings */
+		for (iterate = 1; iterate < argc; iterate ++)
+		{
+			/* prints all the arguments that's passed through */
+			printf ("%s\n", argv[iterate]);
+		}
+	}
+	/* returns nothing if successful */
+	return (0);
+}
