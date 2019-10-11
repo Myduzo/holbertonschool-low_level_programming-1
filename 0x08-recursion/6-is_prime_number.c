69c2fb6aea0d50d49f3f9360ffec83fbe3bd2d4a
jenntang1 <1039@holbertonschool.com>
2019-10-11 02:19:43 +0000

    Understanding Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,18 +9,27 @@
  */
 int helper(int n, int i)
 {
-	if (n <= 2)
+	/* BASE CASE */
+	/* if given integer is less than or equal to 2 */
+	if (n <= 1)
 	{
-		return (n == 2);
-	}
-	if (n % i == 0)
-	{
+		/* then, return 2 because it's the first prime */
 		return (0);
 	}
+	/* if given integer is even */
+	if (n % i == 0)
+	{
+		/* then, return 0 because prime #s are odd (except 2) */
+		return (0);
+	}
+	/* if there are more than one factor */
+	/* Ex: 4 = 2*2 and 1*4 */
 	if (i * i > n)
 	{
+		/* then, return 1 because it's prime */
 		return (1);
 	}
+	/* recursion  */
 	return (helper(n, i + 1));
 }
 
@@ -32,9 +41,13 @@ int helper(int n, int i)
  */
 int is_prime_number(int n)
 {
+	/* BASE CASE */
+	/* if given integer is 0 */
 	if (n == 0)
 	{
+		/* then, return 0 because 0 isn't a prime */
 		return (0);
 	}
+	/* check for prime starting at 2 and return if prime */
 	return (helper(n, 2));
 }
