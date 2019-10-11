193eb75b72671eeac1c55df72470436f19b0b8d2
jenntang1 <1039@holbertonschool.com>
2019-10-11 23:18:31 +0000

    Understanding arguments to main

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,22 +14,30 @@ int main(int argc, char *argv[])
 	/* declare counters */
 	int iterate1;
 	int iterate2;
+
+	/* declare sum to start summing at zero */
 	int sum = 0;
 
+	/* if no number is passed through  */
 	if (argc < 0)
 	{
 		/* then, print zero */
 		printf("0\n");
 	}
+	/* iterate through all numbers that are passed through  */
 	for (iterate1 = 1; iterate1 < argc; iterate1++)
 	{
-		/* sum them */
+		/* sum the array */
 		/* atoi converts str to int */
 		sum += atoi(argv[iterate1]);
+		/* for edge case purpose, create a 2D array */
+		/* iterate through all numbers of both arrays */
 		for (iterate2 = 0; argv[iterate1][iterate2] != '\0'; iterate2++)
 		{
+			/* if a non-digit is found */
 			if (!(isdigit(argv[iterate1][iterate2])))
 			{
+				/* print Error */
 				printf("Error\n");
 				return (0);
 			}
