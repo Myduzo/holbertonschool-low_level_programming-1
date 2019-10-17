6c9fcf84a1a7402a0123ead4375bd76fe7a08715
jenntang1 <1039@holbertonschool.com>
2019-10-17 01:23:07 +0000

    Understanding malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,20 +11,16 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	/* declare a pointer for the concatenated string */
+	/* declare a pointer for the concatenate string */
 	char *str;
 	/* declare iteration variables */
-	int len1 = 0, len2 = 0, iterate1 = 0, iterate2 = 0;
+	int len1 = 0, len2 = 0;
+	int i1 = 0, i2 = 0;
 	/* if strings are NULL, then return empty */
-	/* return pointer to empty string */
 	if (s1 == NULL)
-	{
-		s1 = '\0';
-	}
+		s1[0] = '\0';
 	if (s2 == NULL)
-	{
-		s2 = '\0';
-	}
+		s2[0] = '\0';
 	/* find the length of the strings */
 	for (; s1[len1] != '\0'; len1++)
 	{
@@ -32,22 +28,24 @@ char *str_concat(char *s1, char *s2)
 	for (; s2[len2] != '\0'; len2++)
 	{
 	}
-	/* allocate memory for the str array */
-	str = malloc((s1[len1] + s2[len2] + 1) * sizeof(char));
+	/* allocate memory for the concatenate string */
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
 	/* return NULL if malloc fails */
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-	/* iterate through the strings */
-	/* copy the strings into str array */
-	for (; iterate1 < s1[len1]; iterate1++)
+	/* if strings are empty, then str will be too */
+	if ((len1 + len2) == 0)
+		str[0] = '\0';
+	/* iterate through strings and */
+	/* copy them into the allocated memory */
+	for (; i1 < len1; i1++)
 	{
-		str[iterate1] = s1[iterate1];
+		str[i1] = s1[i1];
 	}
-	for (; iterate2 <= s2[len2]; iterate2++)
+	for (; i2 <= len2; i2++)
 	{
-		str[iterate1 + iterate2] = s2[iterate2];
+		str[i1 + i2] = s2[i2];
 	}
+	/* returns pointer to string */
 	return (str);
 }
