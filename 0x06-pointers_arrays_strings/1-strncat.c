ddb3fdcb3ed771b20514e8ab177c2a1bf5327b47
jenntang1 <1039@holbertonschool.com>
2019-10-18 12:42:44 +0000

    Understanding pointers, arrays and strings in C

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -31,21 +31,3 @@ char *_strncat(char *dest, char *src, int n)
 	dest[len + count] = '\0';
 	return (dest);
 }
-
-/**
- * _strlen - function that returns the length of a string
- * @s: characters
- * Description: Look up man strlen for reference
- * Return: Always 0 is success
- */
-int _strlen(char *s)
-{
-	int len = 0;
-	int count;
-
-	for (count = 0; s[count] != '\0'; count++)
-	{
-		len++;
-	}
-	return (len);
-}
