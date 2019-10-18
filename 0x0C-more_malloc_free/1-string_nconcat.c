8401fc01a51398fb3ca2c62fe94f73c6a426c8c0
jenntang1 <1039@holbertonschool.com>
2019-10-18 02:30:06 +0000

    Understanding malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,22 +12,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	/* declare variable to hold concatenated string */
 	char *str;
+	/* declare iteration variables */
 	unsigned int len1 = 0, len2 = 0, i1 = 0, i2 = 0;
 
+	/* if NULL is passed, treat as an empty string */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	/* find the lengths of each string */
 	for (; s1[len1] != '\0'; len1++)
 		;
 	for (; s2[len2] != '\0'; len2++)
 		;
+	/* if there's space, concatenate all of s2 */
 	if (n >= len2)
 		n = len2;
+	/* allocate memory */
 	str = malloc(sizeof(char) * (len1 + n + 1));
+	/* return NULL if malloc fails */
 	if (str == NULL)
 		return (NULL);
+	/* iterate through both strings */
+	/* copy into concatenated string */
 	for (; i1 < len1; i1++)
 	{
 		str[i1] = s1[i1];
@@ -36,6 +45,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[i1] = s2[i2];
 	}
+	/* add null byte to concatenated string */
 	str[len1 + n] = '\0';
+	/* return pointer to allocated memory */
 	return (str + '\0');
 }
