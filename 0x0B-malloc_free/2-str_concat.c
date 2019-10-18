b0d97637722b8e810deb15c03d1f39551b1af6de
jenntang1 <1039@holbertonschool.com>
2019-10-18 02:35:49 +0000

    Understanding malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,28 +20,26 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	else
+	/* find the length of the strings */
+	for (; s1[len1] != '\0'; len1++)
+		;
+	for (; s2[len2] != '\0'; len2++)
+		;
+	/* allocate memory for the concatenate string */
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
+	/* return NULL if malloc fails */
+	if (str == NULL)
+		return (NULL);
+	/* iterate through strings and */
+	/* copy them into the allocated memory */
+	for (; i1 < len1; i1++)
 	{
-		/* find the length of the strings */
-		for (; s1[len1] != '\0'; len1++)
-			;
-		for (; s2[len2] != '\0'; len2++)
-			;
-		/* allocate memory for the concatenate string */
-		str = malloc(sizeof(char) * (len1 + len2 + 1));
-		/* return NULL if malloc fails */
-		if (str == NULL)
-			return (NULL);
-		/* iterate through strings and */
-		/* copy them into the allocated memory */
-		for (; i1 < len1; i1++)
-		{
-			str[i1] = s1[i1];
-		}
-		for (; i2 < len2; i1++, i2++)
-		{
-			str[i1] = s2[i2];
-		}
+		str[i1] = s1[i1];
 	}
-	return (str);
+	for (; i2 < len2; i2++, i1++)
+	{
+		str[i1] = s2[i2];
+	}
+	str[len1 + len2] = '\0';
+	return (str + '\0');
 }
