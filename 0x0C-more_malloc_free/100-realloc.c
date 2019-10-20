e111e7499d4d8ebbac6599460f2621052e719b69
jenntang1 <1039@holbertonschool.com>
2019-10-20 04:35:22 +0000

    Understanding malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_realloc - function that reallocates memory
+ * @ptr: pointer to the memory
+ * @old_size: size in bytes
+ * @new_size: size in bytes
+ * Description: Lookup man realloc
+ * Return: NULL if fail
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	/* declare temporary pointer */
+	void *array;
+	/* "added" memory shouldn't be initialized */
+	if (new_size > old_size)
+	{
+		array = malloc(new_size);
+		free(ptr);
+		return (array);
+	}
+	/* if reallloc fails the original block is untouched */
+	/* it's not freed or moved, just return ptr */
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	/* call is equivalent to malloc(new_size) */
+	if (ptr == NULL)
+	{
+		array = malloc(new_size);
+		free(array);
+		return (array);
+	}
+	/* call is equivalent to free(ptr) and return NULL */
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	/* return ptr to newly allocated memory */
+	return (ptr);
+}
