45baf897e91e2ba944672430059336d0b248552c
jenntang1 <1039@holbertonschool.com>
2019-10-20 04:37:54 +0000

    Understanding malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	/* declare temporary pointer */
-	unsigned int *array;
+	char *array;
 	/* "added" memory shouldn't be initialized */
 	if (new_size > old_size)
 	{
