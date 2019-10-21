3f518cfffd1517c908b75538de92cd62387a488d
jenntang1 <1039@holbertonschool.com>
2019-10-21 23:59:36 +0000

    Understanding struct and typedef in C programming

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,40 +9,42 @@
  */
 void print_dog(struct dog *d)
 {
-	/* declare variable for printing nothing */
+	/* declare variable to print nothing */
 	char *print = NULL;
-
 	/* if d is NULL, print nothing */
 	if (d == NULL)
 	{
 		printf("%s\n", print);
 	}
-	/* print Name */
-	if (d->name)
+	/* if d is not NULL */
+	else if (d)
 	{
-		printf("Name: %s\n", d->name);
+		/* print Name */
+		if (d->name)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		/* print Age */
+		if (d->age)
+		{
+			printf("Age: %f\n", d->age);
+		}
+		else
+		{
+			printf("Age: (nil)\n");
+		}
+		/* print Owner */
+		if (d->owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
 	}
-	else
-	{
-		printf("Name: (nil)\n");
-	}
-	/* print Age */
-	if (d->age)
-	{
-		printf("Age: %f\n", d->age);
-	}
-	else
-	{
-		printf("Age: (nil)\n");
-	}
-	/* print Owner */
-	if (d->owner)
-	{
-		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("Owner: (nil)\n");
-	}
-
 }
