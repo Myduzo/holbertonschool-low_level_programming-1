491bec6dfc2f2d7ee013af6d6445bc6827fd7227
jenntang1 <1039@holbertonschool.com>
2019-10-22 21:31:46 +0000

    Understanding struct and typedef in C programming

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,24 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - function that returns the length of a string
+ * @s: characters
+ * Description: Look up man strlen for reference
+ * Return: Always 0 is success
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * *new_dog - function that creates a new dog
  * @name: char type
  * @age: float type
@@ -14,20 +32,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	/* declare pointer to struct */
 	dog_t *ptr;
-
-	/* allocate memory for struct */
-	ptr = (dog_t *)malloc(sizeof(dog_t));
-
-	/* store a copy of each element */
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
-
-	/* if malloc fails, return null */
-	/* if success, return pointer */
+	/* declare iteration variables */
+	int i1, i2;
+	/**
+	 * allocate memory for struct and its members
+	 * use _strlen to get the size
+	 * free allocated memory where makes sense
+	 * return NUll if fail
+	 */
+	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
+		return (NULL);
+	ptr->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (ptr->name == NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
+	ptr->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
+	/* store a copy of name & owner */
+	for (i1 = 0; name[i1] != '\0'; i1++)
+		ptr->name[i1] = name[i1];
+	for (i2 = 0; owner[i2] != '\0'; i2++)
+		ptr->owner[i2] = owner[i2];
+	/* point to age */
+	ptr->age = age;
+	/* return pointer to allocated memory */
 	return (ptr);
 }
