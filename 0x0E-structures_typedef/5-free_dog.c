c06a72c8e297021bae3311050b3cfdc1faa0036b
jenntang1 <1039@holbertonschool.com>
2019-10-22 02:13:20 +0000

    Understanding struct and typedef in C programming

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees allocated memory
+ * @d: pointer to struct
+ * Description: Free allocated memory
+ * Return: NULL if fail
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
