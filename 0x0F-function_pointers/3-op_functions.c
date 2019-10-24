d71848b427cf63afce1caf7b8fc9d0c9c1672049
jenntang1 <1039@holbertonschool.com>
2019-10-24 00:29:06 +0000

    1. Pushing, Create file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - function for adding
+ * @a: integer type
+ * @b: integer type
+ * Description: This program will return
+ * a sum after adding of a & b.
+ * Return: Sum
+ */
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function for subtracting
+ * @a: integer type
+ * @b: integer type
+ * Description: This program will return
+ * a difference after subtraction of a & b.
+ * Return: Difference
+ */
+int op_sub(int a, int b);
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function for multiplying
+ * @a: integer type
+ * @b: integer type
+ * Description: This program will return
+ * a product after multiplication of a & b.
+ * Return: Product
+ */
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function for dividing
+ * @a: integer type
+ * @b: integer type
+ * Description: This program will return
+ * a quotient after division of a by b.
+ * Return: Quotient
+ */
+int op_div(int a, int b);
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function for a remainder
+ * @a: integer type
+ * @b: integer type
+ * Description: This program will return 
+ * a remainder as a result of division.
+ * Return: Remainder
+ */
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
