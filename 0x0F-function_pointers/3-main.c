9c70d8bce7ef735b6773d9cb2c4fa4481ae02ef1
jenntang1 <1039@holbertonschool.com>
2019-10-24 21:37:18 +0000

    2. Pushing, Remove standard libraries Update program code to fulfill requirements

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,19 +1,44 @@
-#include "calc.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "3-calc.h"
 
 /**
  * main - function for executing arithmetic operations
- * 
- * Description: This program will print the result of an arithmetic operation
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Description: This program will print the result of
+ * an arithmetic operation
  * Return: 0 for success
  */
-int main(void);
+int main(int argc, char *argv[])
 {
-	int result;
+	/* declare function pointer */
+	int (*result)(int, int);
+	/* declare & initialize returning arguments */
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
 
-	result = get_op_func(atoi[s], a, b);
-	printf("%d\n", result);
+	/* if number of argc is wrong, print Error & exit */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/* assign function pointer to function */
+	result = get_op_func(argv[2]);
+	/* if operator is not found, print Error & exit */
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	/* if trying to divide by 0, print Error & exit */
+	/* can also write argv[2][0] */
+	if ((atoi(argv[3]) == 0) && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/* print result of arithmetic operation */
+	printf("%d\n", result(num1, num2));
 	return (0);
 }
 
