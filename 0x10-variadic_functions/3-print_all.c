33647fd75abbc1f47fc08555daf4cd1bbc0a505e
jenntang1 <1039@holbertonschool.com>
2019-10-28 03:07:54 +0000

    1. Pushing, Creating initial file, Containing function to print any data type, jenntang1

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ * @format: list of argument types
+ * Description: 3. To be is a to be the value of a variable
+ * This program will print any data type follow by a new line.
+ * Return: N/A
+ */
+void print_all(const char * const format, ...)
+{
+	const char *ptr;
+	
+	va_list argList;
+	
+	va_start(argList, format);
+	while (!ptr)
+	{
+		ptr = va_arg(argList, char*);
+		switch (ptr)
+		{
+			case 'c':
+				printf("%c", va_arg(argList, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(argList, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(argList, double));
+				break;
+			case 's':
+			{
+				if (format == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", ptr);
+				break;
+			}
+			default:
+				ptr = 0;
+				break;
+		}
+		ptr++;
+		printf(", ");
+	}
+	va_end(argList);
+	printf("\n");
+}
