a5447886d0b53385ed4651f5b6eb7959b9c31b47
jenntang1 <1039@holbertonschool.com>
2019-11-01 03:14:37 +0000

    3. Pushing, Updating to account for edge case(s), jenntang1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,7 @@
 #include "lists.h"
 
 /* apply constructor attribute so it executes before int main(void) */
-__attribute__ ((constructor))
-
-/* implement startup function */
-void startup(void)
+void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
