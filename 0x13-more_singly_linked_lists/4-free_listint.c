1bb63764b8c6e09ec88fcb32aa5e4a975686761c
jenntang1 <1039@holbertonschool.com>
2019-11-04 19:47:09 +0000

    1. Pushing, Creating function to free linked list, jenntang1

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a linked list
+ * @head: pointer to head of list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	/* declare variable to dereference head */
+	listint_t *next;
+	/**
+	* iterate through the linked link till NULL
+	* freeing the head as it moves
+	* do not need to free integer type
+	*/
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
