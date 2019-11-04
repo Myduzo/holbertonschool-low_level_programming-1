637b9ff54c22e1ff7aad97b2384de2b95be67eee
jenntang1 <1039@holbertonschool.com>
2019-11-04 18:19:54 +0000

    1. Pushing, Creating fuction to print all elements of a list, jenntang1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function that prints all the elements of a list
+ * @h: pointer to head of list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+  /* declare iteration variable */
+  unsigned int i = 0;
+  /* if pointer is not at NULL */
+  /* then, iterate through list */
+  while (h)
+    {
+      /* if pointer is at NULL */
+      /* then, print nil */
+      if (!(h->n))
+	{
+	  printf("(nil)");
+	}
+      else
+	{
+	  /* print after iteration */
+	  printf("%u", h->n);
+	}
+      /* move pointer */
+      h = h->next;
+      /* print new line after printing the elements */
+      printf("\n");
+      /* iterate through list */
+      i++;
+    }
+  /* return the number of nodes */
+  return (i);
+}
