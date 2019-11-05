0df6824c2aa9a3ca1e91aa0c44548bb7ad551585
jenntang1 <1039@holbertonschool.com>
2019-11-05 23:47:41 +0000

    7. Pushing, Updating declaration of pointer variable, jenntang1

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@
 void free_listint2(listint_t **head)
 {
 	/* declare and initialize variable as head */
-	listint_t *pointer = *head;
+	listint_t *pointer;
 	/* declare variable to point to the next node */
 	listint_t *temp;
 	/**
@@ -16,16 +16,17 @@ void free_listint2(listint_t **head)
 	 * 2. moving pointer to the next node
 	 * 3. freeing each node as it iterates
 	 * 4. set pointer to temp which would be at NULL
+	 * 5. set head to NULL
 	 */
 	if (head)
 	{
+		pointer = *head;
 		while (pointer != NULL)
 		{
 			temp = pointer->next;
 			free(pointer);
 			pointer = temp;
 		}
+		*head = NULL;
 	}
-	/* set head to NULL */
-	*head = NULL;
 }
