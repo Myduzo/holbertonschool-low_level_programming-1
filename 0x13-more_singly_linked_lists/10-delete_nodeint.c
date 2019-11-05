3b067c7e22485e8b5d32c29714e0b8b444193244
jenntang1 <1039@holbertonschool.com>
2019-11-05 13:30:29 +0000

    3. Pushing, Updating to include comments, jenntang1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,27 +8,35 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	/* declare variable to hold pointer before free */
 	listint_t *temp = *head;
 	listint_t *next;
-
+	/* declare iteration variable */
 	unsigned int iterate = 0;
-
+	/* if empty, return -1 */
 	if (*head == NULL)
 		return (-1);
-
+	/* if head is the only node in list */
+	/* then, free it */
 	if (index == 0)
 	{
 		*head = temp->next;
 		free(temp);
 		return (1);
 	}
+	/* point to the node before the node to be deleted */
 	while (temp != NULL && iterate < index - 1)
-		temp = temp->next;
 		iterate++;
+		temp = temp->next;
+	/* if given index is more than the number of nodes */
+	/* then, return -1 */
 	if (temp == NULL || temp->next == NULL)
 	{
 		return (-1);
 	}
+	/* hold pointer to the next node to be deleted */
+	/* free the node */
+	/* unlink deleted node from list */
 	next = temp->next->next;
 	free(temp->next);
 	temp->next = next;
