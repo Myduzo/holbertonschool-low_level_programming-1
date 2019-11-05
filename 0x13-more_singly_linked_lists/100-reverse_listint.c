23a5cdd2f4c5bd7c0bf248c55c6e487e30d8358f
jenntang1 <1039@holbertonschool.com>
2019-11-05 16:57:55 +0000

    3. Pushing, Updating to include comments, jenntang1

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,10 +7,17 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	/* declare variable to hold pointer before free */
+	/* declare variables to point to NULL */
 	listint_t *previous = NULL;
 	listint_t *next = NULL;
-
+	/**
+	 * iterate till NULL
+	 * move next pointer to the node after head
+	 * link the first node to NULL
+	 * set previous pointer to head
+	 * set had to next pointer
+	 * eventually, head and next pointer will point to the old NULL
+	 */
 	while (*head != NULL)
 	{
 		next = (*head)->next;
@@ -18,6 +25,7 @@ listint_t *reverse_listint(listint_t **head)
 		previous = *head;
 		*head = next;
 	}
+	/* set pointer to first node and return it */
 	*head = previous;
 	return (previous);
 }
