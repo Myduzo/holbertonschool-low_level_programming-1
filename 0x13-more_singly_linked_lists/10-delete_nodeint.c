7477d23b79d5fb3faf1a92cae239107c1472913c
jenntang1 <1039@holbertonschool.com>
2019-11-05 13:09:45 +0000

    2. Pushing, Updating to be in compliance with Betty style, jenntang1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,12 +10,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
 	listint_t *next;
-	
+
 	unsigned int iterate = 0;
 
 	if (*head == NULL)
 		return (-1);
-		
+
 	if (index == 0)
 	{
 		*head = temp->next;
