152443145a322b1a2162ce19e191bb22c65b9ac0
jenntang1 <1039@holbertonschool.com>
2019-11-05 05:15:01 +0000

    2. Pushing, Updating to accounting for empty list, jenntang1

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,18 +7,25 @@
  */
 void free_listint2(listint_t **head)
 {
+	/* declare variable to temporarily hold current pointer */
+	listint_t *temp = *head;
 	/* declare variable to hold dereferenced head */
 	listint_t *next;
+	/* if head is empty, return nothing */
+	if (*head == NULL)
+	{
+		return;
+	}
 	/**
 	* iterate through the linked link till NULL
-	* freeing the head as it moves
+	* freeing nodes as it iterates
 	* do not need to free integer type
 	*/
-	while (*head != NULL)
+	while (temp != NULL)
 	{
-		next = (*head)->next;
-		free(*head);
-		*head = next;
+		next = temp->next;
+		free(temp);
+		temp = next;
 	}
 	/* set dereferenced head to NULL */
 	*head = NULL;
