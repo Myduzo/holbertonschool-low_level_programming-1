7d3c62ea9816ad5689eaad845cdc12fbbcdfc335
jenntang1 <1039@holbertonschool.com>
2019-11-07 22:02:16 +0000

    1. Pushing, Creating function that returns the value of a bit at a given index, jenntang1

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * get_bit - function that returns the value of a bit
+ * @n: unsigned long int
+ * @index: given index that starts from 0
+ * Description: 2. 10
+ * Return: see below
+ * 1. if successful, the value of bit at index
+ * 2. if error, -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit;
+
+	bit = ((n >> index) & 1);
+	if (bit >= 1)
+	{
+		return (bit);
+	}
+	else if (bit == 0)
+	{
+		return (0);
+	}
+	return (-1);
+}
