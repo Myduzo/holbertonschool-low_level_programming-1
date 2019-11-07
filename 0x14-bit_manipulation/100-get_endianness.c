cc567b3aae799f07998310d4cb37a2889455bc93
jenntang1 <1039@holbertonschool.com>
2019-11-07 23:20:44 +0000

    1. Pushing, Creating function that checks the endianness, jenntang1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ * @void: nothing
+ * Description: 6. Endianness
+ * Return: see below
+ * 1. if big endian, 0
+ * 2. if little endian, 1
+ */
+int get_endianness(void)
+{
+	return (0 | 1);
+}
