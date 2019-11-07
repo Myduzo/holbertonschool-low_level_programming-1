739854e19a8e5a91e75e5e88c85f6782bb86f705
jenntang1 <1039@holbertonschool.com>
2019-11-07 19:33:38 +0000

    5. Pushing, Updating to account for empty n and to include flag for removing leading zeros, jenntang1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,18 +12,28 @@ void print_binary(unsigned long int n)
 	int shifts;
 	/* declare variable to store binary */
 	int binary;
+	/* declare variable to find leading zeros */
+	int flag = 0;
+	/* if zero is given, print zero */
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 	/* iterate backwards through base 10 (0 to 32) */
-	for (shifts = 0; shifts <= 32; shifts++)
+	for (shifts = 32; shifts >= 0; shifts--)
 	{
 		/* n will right shift by the number of shifts */
 		binary = n >> shifts;
-		/* if binary is 1, print 1 */
-		/* if not (all else), print 0 */
-		if (binary == 1)
+		/**
+		 * if binary is bitwise 1 and flag is 1, then print 1
+		 * if not (all else), print 0
+		 */
+		if (binary & 1)
 		{
 			_putchar('1');
+			flag = 1;
 		}
-		else
+		else if (flag)
 		{
 			_putchar('0');
 		}
