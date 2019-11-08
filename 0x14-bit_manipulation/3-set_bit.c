3d943cfa3ee6f924805f5a6a9aac8f37efeb9f5d
jenntang1 <1039@holbertonschool.com>
2019-11-08 21:22:32 +0000

    2. Pushing, Updating to simplifying program code, jenntang1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,8 +2,8 @@
 
 /**
  * set_bit - function that sets the value of a bit to 1
- * @n: unsigned long int
- * @index: given index that starts from 0
+ * @n: pointer to unsigned long int
+ * @index: index that starts from 0
  * Description: 3. 11
  * Return: see below
  * 1. if successful, 1
@@ -11,14 +11,23 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	/* declare variable to hold value of bit */
-	unsigned int new_n = *n;
-	unsigned int value;
-
-	value = ((1 << index) | new_n);
-	if (value)
+	/* if index is greater than 64 bits */
+	/* then, return -1 */
+	if (index > 64)
+	{
+		return (-1);
+	}
+	/**
+	 * 1. left shift by index
+	 * 2. take the value of n
+	 * 3. compare it using OR operator
+	 * 4. compare each binary digit to 1
+	 * 5. if true, return 1
+	 * 6. if false, return 0
+	 * 7. if overall function is successful, return 1
+	 */
+	*n |= 1 << index;
 	{
 		return (1);
 	}
-	return (-1);
 }
