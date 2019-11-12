a26a13e75623e3bd239b2260bfc7619feb77eead
jenntang1 <1039@holbertonschool.com>
2019-11-12 20:31:04 +0000

    2. Pushing, Updating to include standard library headers, jenntang1

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
 int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
