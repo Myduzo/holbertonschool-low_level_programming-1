841a82099045b7c4780d7949216b0fa4b2805ebf
jenntang1 <1039@holbertonschool.com>
2019-11-13 17:12:35 +0000

    3. Pushing, Updating to add if statements to check for system call fails, jenntang1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,31 +12,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* declare variable for buffer, file descriptor & size of text file */
-	char *buf;
-	ssize_t fd, size;
-	/* per requirement, if filename is NULL, return 0 */
+	/* declare variable for file descriptor */
+	int fd;
+	/* declare variables to check if close, read or write fails */
+	ssize_t check_c, check_r, check_w;
+	/* allocate memory for buffer */
+	char *buf = malloc(sizeof(char) * (letters));
+	/* requirement: if filename is NULL, return 0 */
 	if (filename == NULL)
 		return (0);
-	/* allocate memory for buffer */
-	buf = malloc(sizeof(ssize_t) * (letters));
-	if (buf == NULL)
+	/* open file but if it fails or empty, return 0 */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	/* open text file but if it's empty, exit and return 0 */
-	fd = open(filename, O_RDWR);
-	if (fd < 0)
-	{
-		exit(1);
+	/* read file but if it fails, return 0 */
+	check_r = read(fd, buf, letters);
+	if (check_r == -1)
 		return (0);
-	}
-	/* read text file to get the size of it */
-	/* and add null byte at end */
-	size = read(fd, buf, letters);
-	buf[size] = '\0';
-	/* write text to standard output */
-	write(STDOUT_FILENO, buf, letters);
-	/* close file */
-	close(fd);
-	/* per requirement, if successful, return letters */
-	return (letters);
+	/* write text to standard output but if it fails, return 0 */
+	check_w = write(STDOUT_FILENO, buf, check_r);
+	if (check_w == -1)
+		return (0);
+	/* close file but if it fails, return 0 */
+	check_c = close(fd);
+	if (check_c == -1)
+		return (0);
+	/* requirement: if successful, return actual number of letters */
+	return (check_r);
 }
