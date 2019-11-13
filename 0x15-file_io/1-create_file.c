a53c82105ea204cbf7f05b4bf9ef03f75b0da13a
jenntang1 <1039@holbertonschool.com>
2019-11-13 17:27:28 +0000

    5. Pushing, Updating to correct open system call from O_RDWR to O_WONLY, jenntang1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	/* create new file and if it fails, return -1 */
-	newfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	newfile = open(filename, O_CREAT | O_WONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (newfile == -1)
 		return (-1);
 	/* write into new file and if it fails, return -1 */
