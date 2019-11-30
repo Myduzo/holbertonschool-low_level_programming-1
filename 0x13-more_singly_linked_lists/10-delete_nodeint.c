e9856ffc3654eddc5c3cd7da66977174bf44f316
jenntang1 <1039@holbertonschool.com>
2019-11-30 03:25:20 +0000

    5. Pushing, Updating to change declared variables in order to reduce confusion, Updating to also reorder conditions, jenntang1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,37 +8,38 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	/* declare variable to hold pointer before free */
-	listint_t *temp = *head;
-	listint_t *next;
+	/* declare variable pointers to free later */
+	listint_t *pointer = *head;
+	listint_t *temp;
 	/* declare iteration variable */
 	unsigned int iterate = 0;
+	/* if index is more than the number of nodes */
+	/* then, return -1 */
+	if (pointer == NULL || pointer->next == NULL)
+		return (-1);
 	/* if empty, return -1 */
 	if (*head == NULL)
 		return (-1);
-	/* if head is the only node in list */
-	/* then, free it */
+	/* if head is the only node, then free it */
 	if (index == 0)
 	{
-		*head = temp->next;
-		free(temp);
+		*head = pointer->next;
+		free(pointer);
 		return (1);
 	}
 	/* point to the node before the node to be deleted */
-	while (temp != NULL && iterate < index - 1)
-		temp = temp->next;
-		iterate++;
-	/* if given index is more than the number of nodes */
-	/* then, return -1 */
-	if (temp == NULL || temp->next == NULL)
+	while ((pointer != NULL) && (iterate < index - 1))
 	{
-		return (-1);
+		pointer = pointer->next;
+		iterate++;
 	}
-	/* hold pointer to the next node to be deleted */
-	/* free the node */
-	/* unlink deleted node from list */
-	next = temp->next->next;
-	free(temp->next);
-	temp->next = next;
+	/**
+	 * 1. hold pointer to the next node to be deleted
+	 * 2. unlink deleted node from list
+	 * 3. free pointers
+	 */
+	temp = pointer->next->next;
+	free(pointer->next);
+	pointer->next = temp;
 	return (1);
 }
