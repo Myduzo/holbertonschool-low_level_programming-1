eb904bf611126effd5e672aef4c289debb7f9f5b
jenntang1 <1039@holbertonschool.com>
2019-12-14 19:49:25 +0000

    3. Updating to correct if there's an empty list, then add new node, jenntang1

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,21 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - function that returns the length of a string
- * @s: string
- * Return: length
- */
-unsigned int _strlen(const char *s)
-{
-	unsigned int len = 0;
-	unsigned int count;
-
-	for (count = 0; s[count] != '\0'; count++)
-		len++;
-	return (len);
-}
-
-/**
  * add_dnodeint_end - function that adds a new node at the end of a DLL
  * @head: pointer to head of DLL
  * @n: element of integer type
@@ -28,42 +13,39 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	/* declare variable for new node */
 	dlistint_t *new_node;
-	/* declare variable to iterate to last node */
-	dlistint_t *last = *head;
+	/* declare variable to iterate to end */
+	dlistint_t *current = *head;
 	/* allocate memory for new node but if malloc fails, return NULL */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
+	{
 		return (NULL);
-	/**
-	 * 1. create the new node
-	 * 2. point to NULL after it and point to NULL before it
-	 */
+	}
+	/* create new node, point to NULL after it and point to NULL before it */
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
 	/**
-	 * 1. if list is empty, then new node is head
-	 * 2. point new node next to head
-	 * 3. point new node prev from NULL to the new node
-	 * 4. set new node as head
-	 * 5. return address of the new element
+	 * 1. if there's an empty list, set new node as head and return its address
+	 * 2. point new node next to head and new node prev from NULL to the new node
+	 * 3. set new node as head and return address of the new element
 	 */
 	if (*head == NULL)
 	{
-		new_node->next = *head;
-		new_node->prev = new_node;
 		*head = new_node;
 		return (new_node);
 	}
 	/**
 	 * 1. iterate to the end of the list
-	 * 2. point current node next to new node
-	 * 3. point new node prev to current node
-	 * 4. return address of the new element
+	 * 2. point current node next to new node and new node prev to current node
+	 * 3. return address of the new element
 	 */
-	while (last->next != NULL)
-		last = last->next;
-	last->next = new_node;
-	new_node->prev = last;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	new_node->prev = current;
+	current->next = new_node;
+	new_node->next = NULL;
 	return (new_node);
 }
