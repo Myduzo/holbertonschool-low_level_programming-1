9a8aee85c9835718309a6a6dac6a4b8cbbc2963d
jenntang1 <1039@holbertonschool.com>
2019-12-15 23:03:11 +0000

    Updating to correct valgrind for memory leaks, jenntang1

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,16 +15,29 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current = *head;
 	/* declare iteration variable */
 	unsigned int iterate = 0;
-	/* if empty, return -1 */
+	/* if empty DLL, return -1 */
 	if (*head == NULL)
-	{
 		return (-1);
-	}
-	/* if head is the only node, then, free it */
+	/* if head is the only node */
 	if (index == 0)
 	{
 		*head = current->next;
 		free(current);
+		return (1);
+	}
+	/* if the node after head needs to be deleted */
+	if (index == 1)
+	{
+		*head = current->next->next;
+		free(current->next);
+		current->next->prev = *head;
+		return (1);
+	}
+	/* if last node needs to be deleted */
+	if (current->next == NULL)
+	{
+		free(current->next);
+		current->prev->next = NULL;
 		return (1);
 	}
 	/* iterate to the node before the node to be deleted */
@@ -35,15 +48,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	/* if index is more than number of nodes, then, return -1 */
 	if (current == NULL || current->next == NULL)
-	{
 		return (-1);
-	}
-	/**
-	 * 1. point to the node after the one to be deleted
-	 * 2. free node
-	 * 3. point the node that's after the deleted one to current
-	 * 4. return 1
-	 */
+	/* point to node after the one to be deleted and before */
 	current->next = current->next->next;
 	free(current->next->prev);
 	current->next->prev = current;
