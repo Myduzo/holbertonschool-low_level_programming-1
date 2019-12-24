f1de7550a218fc4d30085c10ae2c6ea6a46e9ba7
jenntang1 <1039@holbertonschool.com>
2019-12-24 03:29:05 +0000

    4. Updating to correct pointers in order to delete head node, jenntang1

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,29 +3,23 @@
 /**
  * pop_listint - function that deletes the head node
  * @head: pointer to head of list
- * Return: below
+ * Return: see below
  * 1. upon success, return data of head
  * 2. upon fail, return 0
  */
 int pop_listint(listint_t **head)
 {
 	/* declare variable to point to head node */
-	listint_t *pointer;
-	/* if empty, return 0 */
+	listint_t *current = *head;
+	/* if it's an empty list, return 0 */
 	if (*head == NULL)
 		return (0);
 	/**
-	 * 1. if the list is not empty
-	 * 2. assign first node to pointer
-	 * 3. the next node will become the new head
-	 * 4. free the head
-	 * 5. return data of old head
+	 * 1. move head pointer to point to the next node
+	 * 2. free current pointer that points to head
+	 * 3. return data of old head
 	 */
-	if (*head != NULL)
-	{
-		pointer = *head;
-		*head = (*head)->next;
-		free(pointer);
-	}
-	return (pointer->n);
+	*head = current->next;
+	free(current);
+	return (current->n);
 }
