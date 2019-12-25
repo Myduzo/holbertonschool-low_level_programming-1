1790993c1227bf623ed864ea3a4bb41315bc05ae
jenntang1 <1039@holbertonschool.com>
2019-12-25 22:24:00 +0000

    2. Updating to add comments, jenntang1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,22 @@
  */
 void times_table(void)
 {
-	int row, col, prod;
-
+	/* declare iteration variables */
+	int row, col;
+	/* declare variable to hold multiplication result */
+	int prod;
+	/**
+	 * 1. iterate from 0-9 & perform multiplication for product
+	 * 2. if product is 0-9, print space & last digit
+	 * 3. if product is greater then 9, print both
+	 * 4. from columns 0-9, print comma & space
+	 */
 	for (row = 0; row <= 9; row++)
 	{
 		for (col = 0; col <= 9; col++)
 		{
 			prod = row * col;
-			if (prod <= 9)
+			if (prod >= 0 && prod <= 9)
 			{
 				_putchar(' ');
 				_putchar((prod % 10) + '0');
