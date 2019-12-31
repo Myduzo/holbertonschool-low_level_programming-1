a7614074fb26c4756df999de68011e05aca9203f
jenntang1 <1039@holbertonschool.com>
2019-12-31 17:19:31 +0000

    6. Updating to correct edge cases, jenntang1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,49 +5,45 @@
  * @head: pointer to head of list
  * @idx: index of list where the new node will be added
  * @n: integer data in node
- * Return: below
+ * Description: 9. Insert
+ * Return: see below
  * 1. upon success, returns address of new node
  * 2. upon fail, returns NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	/* declare variable to iterate to index to insert new node */
 	unsigned int iterate = 0;
-	/* declare variable to temporarily point to head */
-	listint_t *temp = *head;
-	/* declare variable to allocate memory for new node */
-	listint_t *new = malloc(sizeof(listint_t));
-	/* if malloc fails, return NULL */
-	if (!new)
-	{
+	listint_t *current = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
 		return (NULL);
-	}
-	/* if malloc'ed successfully, set new data into new node */
-	new->n = n;
-	/* if list is empty or index is more than the number of nodes, return NULL */
-	if ((!temp) || (!idx))
-	{
-		return (NULL);
-	}
-	/* if index is head then, set new node as head */
+	new_node->n = n;
+	/* if new node is to be inserted at the beginning */
 	if (idx == 0)
 	{
-		new->next = temp;
-		*head = new;
-		return (new);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	/* traverse to the node before index (idx - 1) */
-	/* move temporarily pointer to point to (idx - 1) */
-	while (iterate < (idx - 1))
+	/* if new node is inserted in the middle or end */
+	while ((iterate <= (idx - 1)) && (current != NULL))
 	{
-		temp = temp->next;
+		if ((current->next == NULL) && (iterate == (idx - 1)))
+		{
+			new_node->next = NULL;
+			current->next = new_node;
+			return (new_node);
+		}
+		if (iterate == (idx - 1))
+		{
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+		current = current->next;
 		iterate++;
 	}
-	/* have new node point to the same node as (idx - 1) */
-	/* which is the node after index (idx + 1) */
-	new->next = temp->next;
-	/* point (idx - 1) to the new node */
-	temp->next = new;
-	/* return address of new node */
-	return (new);
+	free(new_node);
+	return (NULL);
 }
