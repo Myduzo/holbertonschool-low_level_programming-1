4c05c4540aaa454b2a329f6e673fa58f4a5629e9
jenntang1 <1039@holbertonschool.com>
2020-01-07 00:48:01 +0000

    5. Updating to correct printing of separator, jenntang1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,8 +5,6 @@
  * @separator: string between integers
  * @n: number of integers
  * Description: 1. To be is to be the value of a variable
- * This program will print numbers separated by
- * special characters follow by a new line.
  * Return: nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -17,32 +15,33 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list argList;
 	/* use va_start to initialize the va_list variable */
 	va_start(argList, n);
-	/* if there's no arguments, then print new line */
+	/* if there's no argument, print new line only */
 	if (n == 0)
 	{
 		printf("\n");
 	}
-	/**
-	 * 1. if there are arguments, iterate through them
-	 * 2. if there's a separator, print args & sep
-	 * 3. if separator is 0, print args only
-	 * 4. if separator is NULL, print args only
-	 */
-	if (n >= 1)
+	/* if there's only one argument, print it & new line */
+	if (n == 1)
+	{
+		printf("%d\n", va_arg(argList, int));
+	}
+	/* if there's no separator but there are arguments */
+	/* if there's a separator and arguments */
+	if (n >= 2)
 	{
 		for (i = 0; i < n; i++)
 		{
+			if (separator == NULL)
+			{
+				printf("%d", va_arg(argList, int));
+			}
 			if (separator)
 			{
 				printf("%d", va_arg(argList, int));
-			}
-			if ((separator != NULL) && (i < (n - 1)))
-			{
-				printf("%s", separator);
-			}
-			if (((separator == 0) || (separator == NULL)) && (i < (n - 1)))
-			{
-				printf("%d", va_arg(argList, int));
+				if (i < (n - 1))
+				{
+					printf("%s", separator);
+				}
 			}
 		}
 		printf("\n");
