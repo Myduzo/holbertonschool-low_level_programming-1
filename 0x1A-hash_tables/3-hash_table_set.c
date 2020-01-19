45d398622d40f8a9c8f17728403192404b5bc502
jenntang1 <1039@holbertonschool.com>
2020-01-19 23:52:12 +0000

    2. Updating to simply add new node at the beginning, jenntang1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *current = NULL;
 	unsigned long int index;
 	/* if hash table or key is not valid */
-	if ((ht == NULL) || (key == NULL))
+	if (!ht || !key)
 		return (0);
 	/* create a node for new key/value pair */
 	new_node = malloc(sizeof(hash_node_t));
@@ -30,19 +30,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	/* set pointer to the index */
 	current = ht->array[index];
-	/* iterate through linked list */
-	while (current != NULL)
-		current = current->next;
-	/* if index is found, then update it */
-	if (current != NULL)
-		current->value = strdup(value);
-	/* if index can't be found, then insert at head */
-	if ((current == ht->array[index]) && (current == NULL))
+	/* if index is empty, then insert new node it */
+	if (current == NULL)
 	{
-		new_node->next = current;
-		current = new_node;
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
 		return (1);
 	}
-	free(new_node);
+	/* if a key is already at the index, then update value */
+	if (current->key == new_node->key)
+	{
+		free(current->value);
+		current->value = new_node->value;
+		return (1);
+	}
 	return (0);
 }
