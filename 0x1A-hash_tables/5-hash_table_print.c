8e8c426ea0910a4e3cde81d162560c37957bb4b1
jenntang1 <1039@holbertonschool.com>
2020-01-20 03:42:34 +0000

    2. Updating to remove trailing comma, jenntang1

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,18 +15,22 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		printf("{}\n");
 	}
-	/* iterate through the linked list & print if not NULL */
+	/**
+	 * 1. iterate through the linked list
+	 * 2. print if not NULL and not the last node
+	 * 3. print if not NULL and last node w/out comma
+	 */
 	if (ht)
 	{
 		printf("{");
 		while (index < ht->size)
 		{
-			if (ht->array[index] != NULL)
+			if ((ht->array[index] != NULL) && (index == ht->size - 1))
 			{
 				printf("'%s': ", ht->array[index]->key);
 				printf("'%s', ", ht->array[index]->value);
 			}
-			if ((ht->array[index] != NULL) && (index == ht->size - 1))
+			if (ht->array[index] != NULL)
 			{
 				printf("'%s': ", ht->array[index]->key);
 				printf("'%s'", ht->array[index]->value);
