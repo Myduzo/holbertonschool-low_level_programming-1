c49f529e928a0b833586a0337117828f9e73d425
jenntang1 <1039@holbertonschool.com>
2020-04-16 22:14:34 +0000

    2. Adding more prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+void print_array(int *array, size_t size);
+
 #endif /* SEARCH_ALGOS_H */
+
